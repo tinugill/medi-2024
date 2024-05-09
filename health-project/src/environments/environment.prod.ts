703765eea7e1a12ed517849947078e539f3367fe
parmjeet@lance.com <104547650+tinugill@users.noreply.github.com>
2024-05-09 18:51:55 +0530

    Delete health-project/src directory

--- health-project/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  base_url: 'https://admin.topmedz.com/api/',
-  auth_url: 'https://admin.topmedz.com/api/customer/',
-  site_url: 'https://admin.topmedz.com/',
-  my_url: 'https://topmedz.com/',
-  zoom_login: 'https://zoom.us/oauth/authorize?client_id=Oxy8lGcZRVuFZOPMqRqXxg&response_type=code&redirect_uri=http%3A%2F%2Fadmin.topmedz.com%2Fapi%2Fzoom-verify'
-
-};
